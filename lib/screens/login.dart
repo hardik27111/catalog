8cb86ea52be077178f295d9b218a19dc448dcee6
Hardik Viradiya <hardikviradiya27@gmail.com>
2022-08-15 18:47:08 +0530

    login page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,60 +1,92 @@
 import 'package:catalog1/utils/routes.dart';
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  String name = "";
+
+  final formKey = GlobalKey<FormState>();
+
+  login() {
+    if (formKey.currentState!.validate()) {
+      Navigator.pushNamed(context, AppRoutes.home);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            Image.asset(
-              'assets/images/login.png',
-              fit: BoxFit.cover,
-              height: 200,
-              width: 200,
-            ),
-            Text(
-              'Welcome',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                        hintText: "Enter username", labelText: "Username"),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        hintText: "Enter password", labelText: "Password"),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  ElevatedButton(
-                      onPressed: () => {
-                        Navigator.pushNamed(context, AppRoutes.home)
-                      },
-                      style: TextButton.styleFrom(
-                        minimumSize: Size(100, 40)
-                      ),
-                      child: Text('Login'))
-                ],
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
               ),
-            ),
-          ],
+              Image.asset(
+                'assets/images/login.png',
+                fit: BoxFit.cover,
+                height: 200,
+                width: 200,
+              ),
+              Text(
+                'Welcome $name',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      onChanged: (value) => {name = value, setState(() => {})},
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Username cannot be empty";
+                        }
+
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          hintText: "Enter username", labelText: "Username"),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Password cannot be empty";
+                        } else if (value.length < 8) {
+                          return "Password length should be at least 8";
+                        }
+
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          hintText: "Enter password", labelText: "Password"),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    ElevatedButton(
+                        onPressed: () => login(),
+                        style: TextButton.styleFrom(minimumSize: Size(100, 40)),
+                        child: Text('Login'))
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
